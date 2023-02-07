69a6be99f3f6f5fb6e6ec209001701d4844ea890
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2023-02-07 09:52:31 +0900

    Feat: next-seo 적용

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,38 @@ import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
 import '../styles/main.css'
+import { DefaultSeo } from "next-seo";
+
+const DEFAULT_SEO = {
+    title: "yewonlog",
+    description: "yewon-Noh's Tech Blog",
+    canonical: "https://www.carrotins.com",
+    openGraph: {
+      type: "website",
+      locale: "ko_KR",
+      url: "https://yewonlog.vercel.app/",
+      title: "yewonlog - Yewon-Noh's Tech Blog",
+      site_name: "yewonlog",
+      images: [
+        {
+          url: "/images/image.png",
+          width: 285,
+          height: 167,
+          alt: "이미지"
+        }
+      ]
+    },
+    twitter: {
+        handle: '@handle',
+        site: '@site',
+        cardType: 'summary_large_image',
+    },
+  };
 
 export default function Nextra({ Component, pageProps }) {
     return (
         <>
+            <DefaultSeo {...DEFAULT_SEO} />
             <Head>
                 <link
                     rel="alternate"
